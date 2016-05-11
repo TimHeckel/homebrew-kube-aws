c816967d23156eadf92dc45250f9cef707f23a0f
Tim Heckel <tim@slatebox.com>
2016-05-10 19:12:40 -0500

    Upgraded to 0.7.0

--- kube-aws.rb
+++ kube-aws.rb
@@ -1,9 +1,9 @@
 class KubeAws < Formula
   desc "Kubernetes on AWS"
   homepage "https://github.com/coreos/coreos-kubernetes/tree/master/multi-node/aws"
-  url "https://github.com/coreos/coreos-kubernetes/releases/download/v0.2.0/kube-aws-darwin-amd64.tar.gz"
-  version "v0.2.0"
-  sha256 "0286313278fc5a1a7e40945bd0e657ab3fb98aba1d9c414939ebccf2854d3def"
+  url "https://github.com/coreos/coreos-kubernetes/releases/download/v0.7.0/kube-aws-darwin-amd64.tar.gz"
+  version "v0.7.0"
+  sha256 "69657672e172933288d4e100e7353c097a20e06565ff78c87a3c4cad3743c7e1"
 
   def install
     bin.install "kube-aws"
